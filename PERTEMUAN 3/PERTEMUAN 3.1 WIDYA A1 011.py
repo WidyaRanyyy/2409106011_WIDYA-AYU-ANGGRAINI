4ea3f9be49a2c1fb689e6b08797308d3da2a2063
WidyaRanyyy <ranyjanoor1@gmail.com>
2024-09-18 20:19:51 +0800

    Add files via upload

---
+++ PERTEMUAN 3/PERTEMUAN 3.1 WIDYA A1 011.py
@@ -0,0 +1,8 @@
+angka = int(input("masukkan angka ="))
+
+if angka >30:
+        print("sapi")
+        if angka >50:
+            print("singa")
+else:
+        print("invalid")
---
+++ PERTEMUAN 3/PERTEMUAN 3.4 WIDYA A1 011.py
@@ -0,0 +1,21 @@
+# cuaca = input("Masukkan cuaca[cerah/hujan]: ")
+
+# if cuaca == "cerah":
+#     print("pergi ke sekolah")
+# elif cuaca == "hujan":
+#     print("diam di rumah")
+# else:
+#     print("invalid")
+
+'''Selain pakai if, bisa juga pakai match'''
+# x = 4
+# match x:
+#     case 0:
+#         print("false")
+#     case 4:
+#         print("true")
+
+'''Operator ternary seperti if tapi cuman 1 baris'''
+# angka = int(input("Masukkan angka: "))
+# result = "Genap" if angka % 2 == 0 else "Ganjil"
+# print(angka, "adalah angka",result)
