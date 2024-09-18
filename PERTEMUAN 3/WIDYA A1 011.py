1996ffdc51ddf853b09b16471bfe6454dff8238f
WidyaRanyyy <ranyjanoor1@gmail.com>
2024-09-18 20:19:20 +0800

    Delete PERTEMUAN 3 directory

--- PERTEMUAN 3/WIDYA A1 011.py
+++
@@ -1,9 +0,0 @@
-a = int(input("masukkan angka pertama =")) 
-b = int(input("masukkan angka kedua="))
-
-if a > b:
-    print("kelinci")
-elif b > a :
-    print("kura kura")
-else : a == b
-    print("kerbau")
