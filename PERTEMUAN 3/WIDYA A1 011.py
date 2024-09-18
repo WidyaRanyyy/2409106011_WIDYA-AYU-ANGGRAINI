e108a40233a46f092d92d4c44cefe3b9ba6fcfe7
WidyaRanyyy <ranyjanoor1@gmail.com>
2024-09-18 20:11:30 +0800

    Add files via upload

---
+++ PERTEMUAN 3/WIDYA A1 011.py
@@ -0,0 +1,9 @@
+a = int(input("masukkan angka pertama =")) 
+b = int(input("masukkan angka kedua="))
+
+if a > b:
+    print("kelinci")
+elif b > a :
+    print("kura kura")
+else : a == b
+    print("kerbau")
