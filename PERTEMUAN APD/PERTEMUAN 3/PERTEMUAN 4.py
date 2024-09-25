c49d863084b9e25ca327253ed49463c745d2eba4
WidyaRanyyy <ranyjanoor1@gmail.com>
2024-09-25 08:28:09 +0800

    Add files via upload

---
+++ PERTEMUAN APD/PERTEMUAN 3/PERTEMUAN 4.py
@@ -0,0 +1,58 @@
+# ulang = 10
+# for i in range(ulang):
+#     print (f"perulangan ke-{str(i)}")
+
+# simpan - [12, "udin petot",14.5, True, 'A']
+# for i in simpan
+#     print(i, end=' ')
+
+# for i  in range(1, 10, 2): #(awal, akhir, lompatan)
+#     print(i)
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# a = 10
+
+# print(f"nilai a ; {a}")
+
+# print ("nilai a : " = str(a))
+
+# jawab = 'ya'
+# hitung = 0
+
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input("Masih Ingin Mengulang? ")
+#     if ulang == "tidak" or ulang =="Tidak":
+#         break
+
+# print(f"Total Perulangan: {hitung}")
+
+# print("Daftar bilangan ganjil dari 1-10”)
+# for i in range(10):
+#     if i % 2 == 0:
+#     print(i)
+
+
+# total = 0
+# while True:
+#     angka = int(input("Masukkan angka (negatif untuk berhenti): "))
+#         if angka < 0:
+#             break
+#     total += angka
+# print(f"Jumlah total angka yang dimasukkan: {total}")
+
+
+# for i  in range(1, 20, 3): #(awal, akhir, lompatan)
+#     if i % 2 == 0:
+#         continue
+#     print(i)
